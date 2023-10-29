9d372458bcec1d113452687ad0c71fc35acaef8f
WarrenT <warrentyler1024@gmail.com>
2023-10-29 01:36:24 +0100

    Update layout and structure in the Companies component for improved responsiveness

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -8,7 +8,7 @@ import {
 const Companies = () => {
   return (
     <div className="w-full bg-white py-[50px]">
-      <div className="max-w-[600px] md:max-w-[1480px] m-auto">
+      <div className="max-w-[600px] md:max-w-[1480px] m-auto px-4 md:px-0">
         <h1 className="text-center text-2xl font-bold text-[#536E95] pb-1">
           Trusted by over 25,000 teams around the world.
         </h1>
@@ -17,10 +17,12 @@ const Companies = () => {
           skills fresh.
         </p>
         <div className="flex justify-center py-8 md:gap-8">
-          <img src={companyLogo1} alt="" />
-          <img src={companyLogo2} alt="" />
-          <img src={companyLogo3} alt="" />
-          <img src={companyLogo4} alt="" />
+          <div className="grid md:grid-cols-4 grid-cols-2 gap-2">
+            <img src={companyLogo1} alt="" />
+            <img src={companyLogo2} alt="" />
+            <img src={companyLogo3} alt="" />
+            <img src={companyLogo4} alt="" />
+          </div>
         </div>
       </div>
     </div>
